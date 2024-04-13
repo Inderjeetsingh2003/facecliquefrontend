e106bd89701325b8b57048bb2ea730fa973eaab4
Zoha1401 <zohasurti2004@gmail.com>
2024-04-13 19:07:15 +0530

    some changes

--- frontend/src/components/TeacherLogin.js
+++ frontend/src/components/TeacherLogin.js
@@ -26,7 +26,7 @@ const json = await response.json();
 console.log("Response:", json); // Add this line to check the response
 if(json.success)
 {
-  localStorage.setItem('token',json.accesstoken)
+  localStorage.setItem('prof-token',json.accesstoken)
   navigate('/profhome')
 
 
